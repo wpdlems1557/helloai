13e03cfdbd2918d29152d4fae1eeac70b8dba753
wpdlems1557 <bjh387245@gmail.com>
2026-03-27 23:56:54 +0900

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,13 +1,23 @@
-import mplfinance as mpf
 import yfinance as yf
 from pandas_datareader import data as pdr
-import yfinance as yf
+import pandas as pd
+import numpy as np  
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-yf.pdr_override()
+# snow의 3년 데이터 CSV추출하기
+# 2020년 9월 16일 상장
+snow_df = yf.download("snow", "2021-02-01", "2024-02-02")
+snow_df.info()
 
-data = pdr.get_data_yahoo("005930.KS", start="2026-01-01", end="2026-3-26")
-print(data)
-# mpf.plot(data, type= "candle")   # mpf.plot(data, type= [line, candle])
-mc = mpf.make_marketcolors(up="r", down="b")
-s = mpf.make_mpf_style(base_mpf_style='starsandstripes', marketcolors=mc)
-mpf.plot(data, type='candle', style=s, volume=True, mav=(5,10,60))
+
+snow_df.to_csv("snow_2y.csv", index=False)
+
+# 3개년 시가 및 종가 데이터 + 거래량 추이
+plt.figure(figsize=(12, 6)) 
+sns.scatterplot(data = snow_df, x = 'Date',y = 'Open',  color='blue', label='open')
+sns.scatterplot(data = snow_df, x = 'Date',y = 'Adj Close', color='orange', label='Adj Close')
+plt.figure(figsize=(12, 6)) 
+sns.lineplot(data = snow_df, x = 'Date',y = 'Volume',  color='red', label='Volume')
+plt.legend()
+plt.show()
